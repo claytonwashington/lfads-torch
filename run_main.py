ba9fb744bce7dc034c0d6e09f9951be9435fca49
claytonwashington <washington.534@osu.edu>
2023-09-14 12:01:29 -0400

    adding new main script

---
+++ run_main.py
@@ -0,0 +1,74 @@
+import subprocess
+import yaml
+
+command_multisession = """
+source activate neurocaas
+echo \"--Moving data into temporary directory and parsing path--\"
+neurocaas-contrib workflow get-data-multi
+
+datapath=$(neurocaas-contrib workflow get-datapath)
+resultpath=$(neurocaas-contrib workflow get-resultpath-tmp)
+
+echo \"--Running AutoLFADS--\"
+source activate lfads-torch
+python /home/ubuntu/lfads-torch/scripts/run_pbt.py $datapath ${} $resultpath
+source deactivate
+
+echo "--Writing results--"
+cd $resultpath/best_model
+zip -r autolfads.zip *
+neurocaas-contrib workflow put-result -r autolfads.zip
+
+source deactivate
+"""
+
+command_single = """
+source activate neurocaas
+echo \"--Moving data into temporary directory and parsing path--\"
+neurocaas-contrib workflow get-data
+
+datapath=$(neurocaas-contrib workflow get-datapath)
+resultpath=$(neurocaas-contrib workflow get-resultpath-tmp)
+
+echo \"--Running AutoLFADS--\"
+source activate lfads-torch
+python /home/ubuntu/lfads-torch/scripts/run_pbt.py $datapath ${} $resultpath
+source deactivate
+
+echo "--Writing results--"
+cd $resultpath/best_model
+zip -r autolfads.zip *
+neurocaas-contrib workflow put-result -r autolfads.zip
+
+source deactivate
+"""
+
+
+def run_command_and_get_output(command):
+    result = subprocess.run(['/bin/bash','-c',command], stdout=subprocess.PIPE)
+    return result.stdout.decode('utf-8').strip()
+
+
+def get_config():
+    command = """
+    echo \"--Moving config file into temporary directory and parsing path--\"
+    source activate neurocaas
+    neurocaas-contrib workflow get-config
+    echo $(neurocaas-contrib workflow get-configpath)
+    source deactivate
+    """
+    return run_command_and_get_output(command)
+
+
+def main():
+    configpath = get_config()
+    with open(configpath, 'r') as file:
+        config = yaml.safe_load(file)
+        if config["multisession"] == "True":
+            subprocess.run(command_multisession.format(configpath))
+        else:
+            subprocess.run(command_single.format(configpath))
+
+
+if __name__ == "__main__":
+    main()
